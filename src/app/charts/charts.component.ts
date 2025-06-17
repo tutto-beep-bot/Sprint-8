a0066e68e2e92b372b54b4d92f286d0172365430
Davi Lessa <davilessa.br@gmail.com>
2025-06-17 19:52:45 +0200

    Map, calendar and charts components were created.

---
+++ src/app/charts/charts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-charts',
+  imports: [],
+  templateUrl: './charts.component.html',
+  styleUrl: './charts.component.scss'
+})
+export class ChartsComponent {
+
+}
