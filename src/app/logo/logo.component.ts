eaa3ac15d0c7899b21a75fab235dff6fda6c3ada
Davi Lessa <davilessa.br@gmail.com>
2025-06-17 19:42:32 +0200

    navbar and logo component have been added.

---
+++ src/app/logo/logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-logo',
+  imports: [RouterModule],
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.scss'
+})
+export class LogoComponent {
+
+}
