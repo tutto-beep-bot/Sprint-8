c7733ca63cf88c267f138509cd8f87c76f0238ad
Davi Lessa <davilessa.br@gmail.com>
2025-06-17 20:42:27 +0200

    table was updated to receive data from database and update itself accordingly.

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Product } from '../interfaces/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
-
+  productsList: Product[] = []
 }
