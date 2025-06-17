78d753fa6702f7744bdc1ebdaafdfc1f9217f039
Davi Lessa <davilessa.br@gmail.com>
2025-06-17 20:12:05 +0200

    all routings are now working for each new component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { MapComponent } from './map/map.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ChartsComponent } from './charts/charts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ProductsListComponent},
+    { path: 'map', component: MapComponent},
+    { path: 'calendar', component: CalendarComponent},
+    { path: 'charts', component: ChartsComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+];
