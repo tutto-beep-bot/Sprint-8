af883c9cf5cb5b792208cc089144261e227a4932
Davi Lessa <davilessa.br@gmail.com>
2025-06-18 18:32:14 +0200

    initial backend setup with express server class

---
+++ server/src/models/server.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from 'express';
+
+class Server {
+    private app: Application;
+    private port: string;
+
+
+    constructor() {
+        this.app = express();
+        this.port = '3001';
+        this.listen();
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Application running in port ${this.port}`)
+        })
+    }
+}
+
+export default Server
