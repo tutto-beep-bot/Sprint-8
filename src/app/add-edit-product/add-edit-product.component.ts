f6051902d6c7d273e2045a1b12abab4fb6a011be
Davi Lessa <davilessa.br@gmail.com>
2025-06-18 18:24:03 +0200

    Form was improved and now saves the value from the user's input

--- src/app/add-edit-product/add-edit-product.component.ts
+++ src/app/add-edit-product/add-edit-product.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'app-add-edit-product',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.scss'
 })
 export class AddEditProductComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: [null, Validators.required],
+      stock: [null, Validators.required]
+    })
+  }
+
+  addProduct(){
+    const product: Product = {
+      name: this.form.value.name,
+      description: this.form.value.description,
+      price: this.form.value.price,
+      stock: this.form.value.stock
+    }
+  }
 }
