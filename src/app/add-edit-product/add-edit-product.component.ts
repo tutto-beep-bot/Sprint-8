8d575be6cfe36be10cc4d57b35d29fb16078ab4a
Davi Lessa <davilessa.br@gmail.com>
2025-06-22 13:56:50 +0200

    CRUD is complete and working. Added Edit product methods.

--- src/app/add-edit-product/add-edit-product.component.ts
+++ src/app/add-edit-product/add-edit-product.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angula
 import { Product } from '../interfaces/product';
 import { ProductService } from '../services/product.service';
 import { ToastrService } from 'ngx-toastr';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-edit-product',
@@ -13,14 +14,24 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AddEditProductComponent {
   form: FormGroup;
+  id: number;
+  operation: string = "Add";
 
-  constructor(private fb: FormBuilder, private _productService: ProductService, private toastr: ToastrService, private router: Router){
+  constructor(private fb: FormBuilder, private _productService: ProductService, private toastr: ToastrService, private router: Router, private aRouter: ActivatedRoute){
     this.form = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
       price: [null, Validators.required],
       stock: [null, Validators.required]
     })
+    this.id = Number(this.aRouter.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+    if(this.id !== 0) {
+      this.operation = 'Edit ';
+      this.getProduct(this.id);
+    }
   }
 
   addProduct(){
@@ -31,11 +42,30 @@ export class AddEditProductComponent {
       stock: this.form.value.stock
     }
 
-    this._productService.saveProduct(product).subscribe(() => {
+    if(this.id !== 0){
+      this._productService.updateProduct(this.id, product).subscribe(() => {
+        this.toastr.info('Product was edited succesfully.', 'Edit complete.');
+        this.router.navigate(['/'])
+      })
+    } else {
+      this._productService.saveProduct(product).subscribe(() => {
       this.toastr.success(`${product.name} was added successfully.`, 'Product added')
       this.router.navigate(['/']);
     })
+    }
+
+    
   }
 
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe((data: Product) => {
+      this.form.setValue({
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.stock
+      })
+    })
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,20 +11,28 @@ export class ProductService {
   private myAppUrl: string; 
   private myApiUrl: string;
 
-  constructor(private http: HttpClient) {
-    this.myAppUrl = environment.endpoint;
-    this.myApiUrl = 'api/products/'
-   }
+	constructor(private http: HttpClient) {
+		this.myAppUrl = environment.endpoint;
+    	this.myApiUrl = 'api/products/'
+   	}
 
-   getListProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.myAppUrl}${this.myApiUrl}`);
-   }
+   	getListProducts(): Observable<Product[]> {
+    	return this.http.get<Product[]>(`${this.myAppUrl}${this.myApiUrl}`);
+   	}
 
-   deleteProduct(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}${id}`)
-   }
+   	deleteProduct(id: number): Observable<void> {
+    	return this.http.delete<void>(`${this.myAppUrl}${this.myApiUrl}${id}`)
+   	}
 
-   saveProduct(product: Product): Observable<void> {
-    return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}`, product)
-   }
+   	saveProduct(product: Product): Observable<void> {
+    	return this.http.post<void>(`${this.myAppUrl}${this.myApiUrl}`, product)
+   	}
+
+   	getProduct(id: number): Observable<Product> {
+    	return this.http.get<Product>(`${this.myAppUrl}${this.myApiUrl}${id}`)
+   	}
+
+	updateProduct(id: number, product: Product): Observable<void> {
+		return this.http.put<void>(`${this.myAppUrl}${this.myApiUrl}${id}`, product)
+	}
 }
