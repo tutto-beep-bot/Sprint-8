30518e5b7816aa322a77d245255c9a87542cb403
Davi Lessa <davilessa.br@gmail.com>
2025-06-23 20:57:52 +0200

    map is now implemented and showing on frontend

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
+import { MapService } from '../services/map.service';
+import { Location } from '../interfaces/location';
 
 @Component({
   selector: 'app-map',
@@ -10,16 +12,25 @@ import * as mapboxgl from 'mapbox-gl';
 export class MapComponent {
   map!: mapboxgl.Map;
 
+  constructor(private _mapService: MapService) {}
+
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiZGF2aXR1dHRvIiwiYSI6ImNtYzlmaWlscTAyYzMyanNkeXRlMGRpeGYifQ.ZUPsfuvZNp45KVF9fnH0uw';
 
     this.map = new mapboxgl.Map({
+      accessToken: 'pk.eyJ1IjoiZGF2aXR1dHRvIiwiYSI6ImNtYzlmaWlscTAyYzMyanNkeXRlMGRpeGYifQ.ZUPsfuvZNp45KVF9fnH0uw',
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [2.1734, 41.3851],
       zoom: 12
     });
 
-    new mapboxgl.Marker().setLngLat([-8.61099, 41.14961]).addTo(this.map);
+    this._mapService.getLocations().subscribe((locations: Location[]) => {
+      locations.forEach(loc => {
+        new mapboxgl.Marker()
+          .setLngLat([loc.longitude, loc.latitude])
+          .setPopup(new mapboxgl.Popup().setText(loc.name))
+          .addTo(this.map)
+      })
+    })
   }
 }
