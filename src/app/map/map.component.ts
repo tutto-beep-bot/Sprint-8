528248427ee98b20e18f5272e062c29bb6d33f5c
Davi Lessa <davilessa.br@gmail.com>
2025-06-23 20:42:03 +0200

    mapbox was initialized and mapComponent logic and service were added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+  map!: mapboxgl.Map;
 
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiZGF2aXR1dHRvIiwiYSI6ImNtYzlmaWlscTAyYzMyanNkeXRlMGRpeGYifQ.ZUPsfuvZNp45KVF9fnH0uw';
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [2.1734, 41.3851],
+      zoom: 12
+    });
+
+    new mapboxgl.Marker().setLngLat([-8.61099, 41.14961]).addTo(this.map);
+  }
 }
