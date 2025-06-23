cb7dfc59feb7ca4ab529c11f582c2dae606283a6
Davi Lessa <davilessa.br@gmail.com>
2025-06-23 21:35:48 +0200

    adding marker is completed on map component.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { MapService } from '../services/map.service';
 import { Location } from '../interfaces/location';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
   map!: mapboxgl.Map;
+  newMarkerName: string = ''
+  newMarkerCoords: [number, number] | null = null;
 
   constructor(private _mapService: MapService) {}
 
@@ -30,7 +33,50 @@ export class MapComponent {
           .setLngLat([loc.longitude, loc.latitude])
           .setPopup(new mapboxgl.Popup().setText(loc.name))
           .addTo(this.map)
-      })
-    })
+      });
+    });
+
+    this.map.on('click', (e: mapboxgl.MapMouseEvent)  => {
+      const mouseEvent = e.originalEvent as MouseEvent | undefined;
+
+      if(!mouseEvent) return;
+
+      const target = mouseEvent.target as HTMLElement | null;
+
+      if (target?.closest('.mapboxgl-marker') || target?.closest('.mapboxgl-popup')) {
+        return;
+      }
+
+      const { lng, lat } = e.lngLat;
+      this.newMarkerCoords = [lng, lat];
+      
+    });
   }
+
+  submitNewLocation() {
+    if (!this.newMarkerCoords || !this.newMarkerCoords) return;
+
+    const [lng, lat] = this.newMarkerCoords;
+    const location = {
+      name: this.newMarkerName,
+      latitude: lat,
+      longitude: lng
+    };
+
+    this._mapService.createLocation(location).subscribe((saved) => {
+      new mapboxgl.Marker()
+        .setLngLat([saved.longitude, saved.latitude])
+        .setPopup(new mapboxgl.Popup().setText(saved.name))
+        .addTo(this.map);
+
+      this.newMarkerCoords = null;
+      this.newMarkerName = ''
+    });
+  }
+
+  cancelNewMarker() {
+    this.newMarkerCoords = null;
+    this.newMarkerName = '';
+}
+
 }
