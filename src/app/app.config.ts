c0877dab0280002f92bcfae82b78e20ac261657f
Davi Lessa <davilessa.br@gmail.com>
2025-06-24 19:37:34 +0200

    after a lot of stress I managed to add a pie chart fully working

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations'
-import { provideToastr } from 'ngx-toastr'
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
 
 
 export const appConfig: ApplicationConfig = {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Chart } from 'chart.js';
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
