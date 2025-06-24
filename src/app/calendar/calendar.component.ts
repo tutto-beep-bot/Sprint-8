1d048684728e87df04ab2fc2b4ea5fc9bf1b0a49
Davi Lessa <davilessa.br@gmail.com>
2025-06-24 16:56:43 +0200

    calendar's logic is added in its first stage.

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
+	calendarOptions: CalendarOptions = {
+		plugins: [dayGridPlugin, interactionPlugin],
+		initialView: 'dayGridMonth',
+		events: [],
+		dateClick: this.handleDateClick.bind(this),
+		eventClick: this.handleEventClick.bind(this),
+	}
 
+	constructor(private http: HttpClient) {}
+
+	ngOnInit() {
+		this.http.get<any[]>('http://localhost:3000/api/deliveries')
+			.subscribe(events => {
+				this.calendarOptions.events = events;
+			});
+	}
+
+	handleDateClick(arg: any) {
+    	alert('Clicked on date: ' + arg.dateStr);
+  	}
+
+  	handleEventClick(arg: any) {
+    	alert('Clicked event: ' + arg.event.title);
+  	}
+
+	handleAddEvent() {
+  		alert('Modal coming soon...');
+	}
 }
