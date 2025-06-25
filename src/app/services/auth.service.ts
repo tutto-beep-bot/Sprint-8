f9eaafe3672ff6695868b0ab3e554d1e73b30abe
Davi Lessa <davilessa.br@gmail.com>
2025-06-25 12:37:49 +0200

    firebase added and service was generated to contain its logic

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { 
+  	getAuth, 
+  	signInWithEmailAndPassword, 
+  	createUserWithEmailAndPassword,
+  	signOut,
+  	onAuthStateChanged,
+  	User
+} from 'firebase/auth';
+import { firebaseConfig } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+	private app = initializeApp(firebaseConfig);
+	private auth = getAuth(this.app);
+	currentUser: User | null = null;
+
+  	constructor() {
+		onAuthStateChanged(this.auth, (user) => {
+			this.currentUser = user;
+		})
+	}
+
+	login(email: string, password: string) {
+		return signInWithEmailAndPassword(this.auth, email, password);
+	}
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,16 @@
+import { ApplicationConfig } from "@angular/core"
+
 export const environment = {
     production: false,
     endpoint: 'http://localhost:3000/'
+}
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAEvCIoq20o8iADUZeHIlqkIkj4vnZHdj0",
+    authDomain: "food-distributor-co.firebaseapp.com",
+    projectId: "food-distributor-co",
+    storageBucket: "food-distributor-co.firebasestorage.app",
+    messagingSenderId: "904920364972",
+    appId: "1:904920364972:web:523c0d1049dfe7aa5aac09",
+    measurementId: "G-PQNK8HJ8YT"
 }
