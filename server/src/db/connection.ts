6373fb804d16fac51481c7f82bb328698eb1fc6c
Davi Lessa <davilessa.br@gmail.com>
2025-06-27 14:47:29 +0200

    included secrets in .env file. I know it was too late and I'll do better next project.

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('market', 'root', 'admin2025', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize( process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD! , {
+    host: process.env.DB_HOST!,
+    dialect: 'mysql',
 });
 
 export default sequelize
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
+import 'dotenv/config';
 import Server from "./models/server";
-import dotenv from 'dotenv'
 
-dotenv.config();
 
 const server = new Server();
